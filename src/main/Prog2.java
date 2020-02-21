52730e7ea2dfc256c5d82792020390324d9e10cf
nocaed <codenamedeacon@outlook.com>
2020-02-21 10:14:31 -0500

    first commit

---
+++ src/main/Prog2.java
@@ -0,0 +1,13 @@
+package main;
+/**
+ * This is the main method of the program, and begins the tuition managing process.
+ * @author Thomas Brewer
+ * @author Michael McLaughlin
+ */
+public class Prog2
+{
+    public static void main(String [] args)
+    {
+        new TuitionManager().run();
+    }
+}
