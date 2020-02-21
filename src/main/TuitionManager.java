756709a4a2eec9002e4b505722d142c5266bf4b4
nocaed <codenamedeacon@outlook.com>
2020-02-21 10:29:51 -0500

    Wrote boilerplate for TuitionManager

--- src/main/TuitionManager.java
+++ src/main/TuitionManager.java
@@ -1,12 +1,53 @@
 package main;
-
+import java.util.Scanner;
 /**
  * @author Thomas Brewer
  * @author Michael McLaughlin
  */
 
 public class TuitionManager {
-    public static void main(String[] args) {
+    Scanner stdin = new Scanner(System.in);
+    StudentList cs213 = new StudentList();
 
+    public void run() {
+        String input;
+        String[] params;
+        boolean done = false;
+
+        while(!done) {
+            input = stdin.nextLine();
+            params = input.split("\\s+");
+            command = input.charAt(0);
+            switch(command) {
+                case 'I': case 'O': case 'N':
+                    // add here
+                    break;
+                case 'R':
+                    // remove here
+                    break;
+                case 'P':
+                    // print here
+                    break;
+                case 'Q':
+                    // terminate here
+                    break;
+                default:
+                    // invalid command
+                    break;
+            }
+            System.out.println("Program terminated");
+        }
+    }
+
+    private void add(String member, char studentType) {
+        // add a new Student here
+    }
+
+    private void remove(String member) {
+        // remove a Student here
+    }
+
+    private void print() {
+        // print all Students here
     }
 }
